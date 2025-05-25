6dc7804c7d0794b14fd70a5e81245b4f7f9deabe
mustafa <musta@safcyber.com>
2025-05-25 06:26:48 +0200

    Add the home page

---
+++ textpattern/config.php
@@ -0,0 +1,9 @@
+<?php
+$txpcfg['db'] = 'textpattern';
+$txpcfg['user'] = 'root';
+$txpcfg['pass'] = '';
+$txpcfg['host'] = 'localhost:3307';
+$txpcfg['table_prefix'] = '';
+$txpcfg['txpath'] = 'C:\xampp\htdocs\textpattern\textpattern';
+$txpcfg['dbcharset'] = 'utf8mb4';
+// For more customization options, please consult config-dist.php file.
